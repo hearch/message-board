6b60ed65a659a7dea88c3637e43e5482b0108f71
yukato <manabi0215@gmail.com>
2015-09-04 02:47:55 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,21 @@
 class MessagesController < ApplicationController
   def index
   end
+  
+  def create
+    @message = Message.new(message.params)
+    @message.save
+    redirect_to root_path , notice: 'メッセージを保存しました'
+  end
+  
+  private
+  def message_params
+    params.require(:message).permit(:name, :body)
+  end
+  
+  class MessagesController < ApplicationController
+    def index
+      @messages = Message.all
+    end
+  
 end
