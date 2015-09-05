28ef51400dae77d5f02f037ffc971ff2130c1270
yukato <manabi0215@gmail.com>
2015-09-05 14:20:37 +0000

    add edit funtcion

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,20 @@
 class MessagesController < ApplicationController
+
+  before_action :set_message, only: [:edit, :update]
+
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
   def index
     # Messageを全て取得する。
     @messages = Message.all
@@ -20,8 +36,14 @@ class MessagesController < ApplicationController
   end
 
   private
+  
   def message_params
     params.require(:message).permit(:name, :body)
   end
   ## ここまで
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
 end
